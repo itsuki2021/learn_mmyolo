bd380d44194f873e475c311edbaa0b2bbd79093f
lst <shitong_liu@163.com>
2022-12-06 15:50:11 +0800

    添加环境验证脚本

---
+++ verify.py
@@ -0,0 +1,11 @@
+from mmdet.apis import init_detector, inference_detector
+from mmyolo.utils import register_all_modules
+
+if __name__ == '__main__':
+    register_all_modules()
+    config_file = 'configs/yolov5_s-v61_syncbn_fast_8xb16-300e_coco.py'
+    checkpoint_file = 'checkpoints/yolov5_s-v61_syncbn_fast_8xb16-300e_coco_20220918_084700-86e02187.pth'
+    model = init_detector(config_file, checkpoint_file, device='cpu')  # or device='cuda:0'
+    result = inference_detector(model, 'data/demo.jpg')
+    print(result)
+    print("Done.")
