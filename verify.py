79ff0e053a8418a23b3487259ccd02d8a975a24c
lst <shitong_liu@163.com>
2022-12-06 16:07:55 +0800

    测试device由CPU改为GPU

--- verify.py
+++ verify.py
@@ -5,7 +5,7 @@ if __name__ == '__main__':
     register_all_modules()
     config_file = 'configs/yolov5_s-v61_syncbn_fast_8xb16-300e_coco.py'
     checkpoint_file = 'checkpoints/yolov5_s-v61_syncbn_fast_8xb16-300e_coco_20220918_084700-86e02187.pth'
-    model = init_detector(config_file, checkpoint_file, device='cpu')  # or device='cuda:0'
+    model = init_detector(config_file, checkpoint_file, device='cuda:0')  # or device='cuda:0'
     result = inference_detector(model, 'data/demo.jpg')
     print(result)
     print("Done.")
